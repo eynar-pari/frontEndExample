0f45a19066b5701b0b0a9594733142c3b386f247
eynar-pari-gmail <eynar.pari@gmail.com>
2018-05-30 08:03:25 -0400

    Added GET logic

--- Libro/src/app/libro.component.ts
+++ Libro/src/app/libro.component.ts
@@ -10,14 +10,22 @@ import { LibroService } from './services/libro.service';
         Name<input type="text" [(ngModel)]="data_name" name="u" placeholder="name" required="required" />
         Autor<input type="text" [(ngModel)]="data_autor" name="u" placeholder="autor" required="required" />
         <input type="button" value="crear" class="btn btn-primary btn-block btn-large" (click)="save($event)" />
-      </div>`
+    
+    <br>  Datos Libro  <br>
+      <br>Libro Name : {{dataTest.name}}
+      <br>Libro Id : {{dataTest.id}}
+      <br>Libro Autor:  {{dataTest.autor}}
+    </div>
+     `
+
 })
 
 export class LibroComponent implements OnInit{
   data_name:any;
   data_id:any;
   data_autor:any;
-  estados:any=[];
+  librosData:any=[];
+  dataTest:any=[];
   constructor(private serviciod: LibroService)
   {
   }
@@ -30,7 +38,7 @@ export class LibroComponent implements OnInit{
       autor:this.data_autor
     }
     this.serviciod.postLibros(newLibro).then(estado => {
-        this.estados.push(estado);
+        this.librosData.push(estado);
       });
 
   }
@@ -40,7 +48,16 @@ export class LibroComponent implements OnInit{
   }
 
   cargar():void{
+    console.log('init ');
+    this.serviciod.getLibros2().subscribe(
+      res => {
+        console.log(res);
+        console.log('data response body'+res['_body']);
+        this.dataTest = JSON.parse(res['_body']);
+        console.log('body '+this.dataTest);
+      }
+    );
+    console.log('final ');
   }
-
 }
 
--- Libro/src/app/services/libro.service.ts
+++ Libro/src/app/services/libro.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@angular/core';
 import {Headers, Http, RequestMethod, RequestOptions} from '@angular/http';
+//import {HttpResponse} from "@angular/common/http";
+//import {Observable} from 'rxjs/Observable';
+import {HttpClient, HttpResponse} from '@angular/common/http';
+import {Observable} from "rxjs/internal/Observable";
 
 @Injectable()
 export class LibroService {
 
   constructor(
     private http: Http) {}
+
+   getLibros2(): Observable<HttpResponse<any>> {
+      return this.http.get('http://localhost:8083/UPB/libros/get',
+         {observe: 'response'});
+    }
+
 
   postLibros(newLibro) {
     let url = 'http://localhost:8083/UPB/libros/post';
@@ -20,7 +30,7 @@ export class LibroService {
       body: data
     });
 
-   return this.http.post(url,data,requestOptions).forEach(res => res.json());
+   return this.http.post(url,data,requestOptions).subscribe(res => res.json());
   }
 }
 
